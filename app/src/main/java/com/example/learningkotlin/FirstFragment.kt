6f66f5aeb6d7da71f8086fecbe104693c5ecbd4c
aagowl <abigail.gatsch@gmail.com>
2023-11-29 19:08:45 -0700

    make things better

--- app/src/main/java/com/example/learningkotlin/FirstFragment.kt
+++ app/src/main/java/com/example/learningkotlin/FirstFragment.kt
@@ -1,12 +1,14 @@
 package com.example.learningkotlin
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.learningkotlin.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -36,7 +38,23 @@ private var _binding: FragmentFirstBinding? = null
         }
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val sharingIntent = Intent(Intent.ACTION_SEND)
+
+            // type of the content to be shared
+            sharingIntent.type = "text/plain"
+
+            // Body of the content
+            val shareBody = "Your Body Here"
+
+            // subject of the content. you can share anything
+            val shareSubject = "Your Subject Here"
+
+            // passing body of the content
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody)
+
+            // passing subject of the content
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject)
+            startActivity(Intent.createChooser(sharingIntent, "Share using"))
         }
 
         binding.buttonThird.setOnClickListener {
