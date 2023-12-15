77984017d3e6ee2b0aad1e9c7828dc74af28e10b
Lorenzo <lorenzo.marez412@gmail.com>
2023-12-15 09:19:00 -0700

    Graphs lookin' betta

--- app/src/main/java/com/example/learningkotlin/ThirdFragment.kt
+++ app/src/main/java/com/example/learningkotlin/ThirdFragment.kt
@@ -1,13 +1,16 @@
 package com.example.learningkotlin
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import com.example.learningkotlin.databinding.FragmentThirdBinding
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 import java.text.SimpleDateFormat
@@ -19,8 +22,8 @@ import java.util.*
 
 class ThirdFragment : Fragment() {
     private var _binding: FragmentThirdBinding? = null
-    // This property is only valid between onCreateView and onDestroyView.
-
+    // This property is only valid between onCreateView and
+    // onDestroyView.
     private val binding get() = _binding!!
 
     lateinit var sharedPreferences: SharedPreferences
@@ -61,41 +64,50 @@ class ThirdFragment : Fragment() {
          */
     }
 
-    private fun loadData(): LineGraphSeries<DataPoint> {
+    private fun loadData(){
         //val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
         //val defaultValue = resources.getInteger(R.integer.saved_high_score_default_key)
         //val highScore = sharedPref.getInt("Biden", defaultValue)
 
-        return LineGraphSeries(
+        /*
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
             arrayOf(
                 // on below line we are adding
                 // each point on our x and y axis.
-                DataPoint(1.0, Q1!!),
-                DataPoint(2.0, Q2!!),
-                DataPoint(3.0, Q3!!),
-                DataPoint(4.0, Q4!!),
-                DataPoint(5.0, plantQ1!!),
-                DataPoint(6.0, plantQ2!!),
-                DataPoint(7.0, plantQ3!!),
+                DataPoint(1.0, 1.0),
+                DataPoint(2.0, 2.0),
+                DataPoint(3.0, 3.0),
+                DataPoint(4.0, 4.0),
+                DataPoint(5.0, 5.0),
+                DataPoint(6.0, 6.0),
+                DataPoint(7.0, 7.0),
             )
         )
+
+        // on below line we are setting color for series.
+        series.color = Color.BLUE
+        // on below line we are adding
+        // data series to our graph view.
+        binding.idGraphViewWellness.addSeries(series)
+
+         */
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var dateTime: Double
+        val dateTime: String
         val calendar: Calendar = Calendar.getInstance()
-        var simpleDateFormat: SimpleDateFormat = SimpleDateFormat("F")
-        dateTime = simpleDateFormat.format(calendar.time).toDouble()
-        binding.idTVHeadWellness.text = dateTime.toString()
+        val simpleDateFormat: SimpleDateFormat = SimpleDateFormat("u")
+        dateTime = simpleDateFormat.format(calendar.time)
+        binding.idTVHeadWellness.text = dateTime
 
         // on below line adding animation
         binding.idGraphViewWellness.animate()
 
-        // on below line we are setting scrollable for point graph view
-        binding.idGraphViewWellness.viewport.isScrollable = true
-
+        // on below line we are setting scrollable
+        // for point graph view
+        //binding.idGraphViewWellness.viewport.isScrollable = false
         // on below line we are setting scalable.
         binding.idGraphViewWellness.viewport.isScalable = true
 
@@ -103,18 +115,43 @@ class ThirdFragment : Fragment() {
         binding.idGraphViewWellness.viewport.setScalableY(true)
 
         // on below line we are setting scrollable y
-        binding.idGraphViewWellness.viewport.setScrollableY(true)
+        //binding.idGraphViewWellness.viewport.setScrollableY(false)
+
+        binding.idGraphViewWellness.viewport.setMaxX(12.0)
+        binding.idGraphViewWellness.viewport.setMinX(0.0)
+
+        binding.idGraphViewWellness.viewport.setMaxY(10.0)
+        binding.idGraphViewWellness.viewport.setMinY(0.0)
+
+        binding.idGraphViewWellness.viewport.isYAxisBoundsManual = true
+        binding.idGraphViewWellness.viewport.isXAxisBoundsManual = true
+
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
+            arrayOf(
+                // on below line we are adding
+                // each point on our x and y axis.
+                DataPoint(0.0, 1.0),
+                DataPoint(2.0, 2.0),
+                DataPoint(4.0, 7.0),
+                DataPoint(6.0, 10.0),
+                DataPoint(8.0, 3.0),
+                DataPoint(10.0, 4.0),
+                DataPoint(12.0, 6.0),
+            )
+        )
+
+        if (binding.idGraphViewWellness.viewport.isYAxisBoundsManual)
+            series.color = Color.BLUE
+        else
+            series.color = Color.RED
+
+
+
+        binding.idGraphViewWellness.addSeries(series)
 
         binding.trackButton.setOnClickListener {
-
-            saveData()
-            val series: LineGraphSeries<DataPoint> = loadData()
-
-            // on below line we are setting color for series.
-            series.color = Color.BLUE
-
-            // on below line we are adding data series to our graph view.
-            binding.idGraphViewWellness.addSeries(series)
+            //saveData()
+            //loadData()
         }
     }
 
